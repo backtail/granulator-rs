ab8e0ac1680bf6151d447483dc16d78465640e5c
backtail <accounts@maxgenson.de>
2022-08-16 14:39:36 +0200

    add check bounds unit test for hann envelope

--- src/grain.rs
+++ src/grain.rs
@@ -273,4 +273,65 @@ pub mod tests {
         );
         check!(g.is_finished(), "Envelope function was not finished yet!",);
     }
+
+    #[test]
+    fn check_bounds_envelope_hann() {
+        const FS: usize = 48_000;
+
+        // use a not "beautiful" to take floating point errors in consideration
+        const GRAIN_SIZE_IN_MS: f32 = 0.9;
+
+        // include zero in front an at the end
+        const TEST_BUFFER_LENGTH: usize = ((GRAIN_SIZE_IN_MS * FS as f32) / 1000.0) as usize + 2;
+
+        // setup grain
+        let mut t = TestGrain::new(FS);
+
+        // create test buffer
+        let mut test_buffer = [0.0_f32; TEST_BUFFER_LENGTH];
+
+        t.activate_grain_with_no_source(GRAIN_SIZE_IN_MS, WindowFunction::Hann);
+
+        // transform TestGrain into real grain
+        let mut g = t.grain;
+
+        // first sample is by definition 0
+        check!(
+            g.envelope_value == 0.0,
+            "First sample was not 0.0, but {}!",
+            g.envelope_value
+        );
+
+        // run through the grain until finished and store its values for further investigation
+        for i in 0..TEST_BUFFER_LENGTH {
+            g.update_envelope();
+            test_buffer[i] = g.envelope_value;
+        }
+
+        for i in 0..TEST_BUFFER_LENGTH {
+            // check upper bound
+            check!(
+                test_buffer[i] <= 1.0,
+                "Envelope grew bigger than 1 at position {}",
+                i
+            );
+
+            // check lower bound
+            check!(
+                test_buffer[i] >= 0.0,
+                "Envelope got negative at position {}",
+                i
+            );
+        }
+
+        // print sine envelope buffer if true
+        check!(true, "{:?}", test_buffer);
+
+        // last sample in buffer has to be 0 if envelope is finished
+        check!(
+            test_buffer[TEST_BUFFER_LENGTH - 1] == 0.0,
+            "Last sample is not 0!",
+        );
+        check!(g.is_finished(), "Envelope function was not finished yet!",);
+    }
 }
