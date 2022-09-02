d4b6b8d182e33f0f14c1f870bd7d5faa62ab49ee
backtail <accounts@maxgenson.de>
2022-09-02 18:16:39 +0200

    added Scheduler struct

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,21 +1,45 @@
 use super::grain::Grain;
-use super::source::Source;
-use super::window_function::WindowFunction;
+use super::manager::{GRAINS, MAX_GRAINS};
 
-// size in ms between 1ms..100ms
-pub fn activate_grain(
-    grain: &mut Grain,
-    size: f32,
-    offset: usize,
-    window: WindowFunction,
-    source: Source,
-    source_length: usize,
-) {
-    grain.activate(size, offset, window, source, source_length);
+use core::time::Duration;
+
+pub struct Scheduler {
+    master_clock_counter: Duration,
+    future_table: [Duration; MAX_GRAINS],
 }
 
-pub fn reactivate_grains(grain: &mut Grain) {
-    if grain.is_finished() {
-        grain.reactivate();
+impl Scheduler {
+    pub fn new() -> Scheduler {
+        Scheduler {
+            master_clock_counter: Duration::ZERO,
+            future_table: [Duration::MAX; MAX_GRAINS],
+        }
+    }
+
+    pub fn update_clock(&mut self) {
+        self.master_clock_counter = self
+            .master_clock_counter
+            .checked_add(Duration::from_millis(1))
+            .unwrap();
+        for i in 0..MAX_GRAINS {
+            if self.future_table[i] <= self.master_clock_counter {
+                self.activate_grain(i);
+            }
+        }
+    }
+
+    // size in ms between 1ms..100ms
+    pub fn activate_grain(&self, id: usize) {
+        GRAINS.lock().get_mut(id).unwrap().activate();
+    }
+
+    pub fn reactivate_grains(&self, grain: &mut Grain) {
+        if grain.is_finished() {
+            grain.reactivate();
+        }
+    }
+
+    pub fn schedule_grain(&mut self, id: usize, delay: Duration) {
+        self.future_table[id] = self.master_clock_counter + delay;
     }
 }
