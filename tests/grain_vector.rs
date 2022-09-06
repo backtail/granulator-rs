44d4c0ed138d66cf8f8ffcfe9a631b3c784856d4
backtail <accounts@maxgenson.de>
2022-09-06 18:20:00 +0200

    add flush grains and get grain unit tests

--- tests/grain_vector.rs
+++ tests/grain_vector.rs
@@ -13,16 +13,27 @@ fn push_and_remove_a_grain() {
     check!(remove_grain(id).is_ok());
 }
 
-// check if grains are being pushed onto vector until it is full
 #[test]
-fn overflow_grain_vector() {
-    for _ in 0..MAX_GRAINS {
+fn flush_grains_vector() {
+    for _ in 0..10 {
         check!(push_grain(get_new_index()).is_ok());
     }
-    check!(push_grain(get_new_index()).is_err());
+    flush_grains();
 }
 
 #[test]
 fn remove_grain_from_empty_vector() {
+    flush_grains();
     check!(remove_grain(get_new_index()).is_err());
+}
+
+#[test]
+fn get_a_grain() {
+    let id = get_new_index();
+
+    check!(push_grain(id).is_ok());
+
+    check!(get_grain(id).is_ok());
+
+    flush_grains();
 }
