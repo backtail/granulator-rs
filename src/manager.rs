e45779e756b215c4792a9bba4dca1ee1db6b59c9
backtail <accounts@maxgenson.de>
2022-09-07 10:13:23 +0200

    refactored granular manager

--- src/manager.rs
+++ src/manager.rs
@@ -1,32 +1,17 @@
-use spin::Mutex;
-
-use super::grain::Grain;
-use super::grain::DEFAULT_GRAIN;
 use super::scheduler::Scheduler;
 
 pub const MAX_GRAINS: usize = 64;
 pub const FS: usize = 48_000;
-pub static GRAINS: Mutex<[Grain; MAX_GRAINS]> = Mutex::new([DEFAULT_GRAIN; MAX_GRAINS]);
 
 pub struct Granulator {
-    pub scheduler: Scheduler, // parameters
+    pub scheduler: Scheduler,
+    // parameters
 }
 
 impl Granulator {
     pub fn new() -> Self {
-        reset();
-        for i in 0..MAX_GRAINS {
-            GRAINS.lock().get_mut(i).unwrap().id = i;
-        }
-
         Granulator {
             scheduler: Scheduler::new(),
         }
-    }
-}
-
-fn reset() {
-    for i in 0..MAX_GRAINS {
-        GRAINS.lock().get_mut(i).unwrap().reset();
     }
 }
--- tests/manager.rs
+++ tests/manager.rs
@@ -1,11 +1,1 @@
-use assert2::*;
-use granulator::manager::{Granulator, GRAINS, MAX_GRAINS};
-
-#[test]
-fn test_ids() {
-    Granulator::new();
-
-    for i in 0..MAX_GRAINS {
-        check!(GRAINS.lock().get(i).unwrap().id == i);
-    }
-}
+// use assert2::*;
