98695c89e57c388092da1cb86341c2889adad85b
backtail <accounts@maxgenson.de>
2022-09-08 14:32:15 +0200

    make scheduler remove finished grains

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -5,20 +5,22 @@ use super::manager::MAX_GRAINS;
 use core::time::Duration;
 use heapless::Vec;
 
-pub struct Future {
-    time: Duration,
+#[derive(Debug)]
+pub struct TimeInfo {
     id: usize,
+    start: Duration,
 }
 
-impl Future {
-    fn new(time: Duration, id: usize) -> Self {
-        Future { time, id }
+impl TimeInfo {
+    fn new(id: usize, start: Duration) -> Self {
+        TimeInfo { id, start }
     }
 }
 
+#[derive(Debug)]
 pub struct Scheduler {
-    master_clock_counter: Duration,
-    future_vector: Vec<Future, MAX_GRAINS>,
+    pub master_clock_counter: Duration,
+    future_vector: Vec<TimeInfo, MAX_GRAINS>,
 }
 
 impl Scheduler {
@@ -30,22 +32,41 @@ impl Scheduler {
     }
 
     pub fn update_clock(&mut self) {
+        // increase counter by 1
         self.master_clock_counter += Duration::from_millis(1);
 
-        for grain in &self.future_vector {
-            if grain.time <= self.master_clock_counter {
-                self.activate_grain(grain.id).ok().unwrap();
+        // remove all finished grains
+        self.future_vector.retain(|future_grain| {
+            let real_grain = grain_vector::get_grain(future_grain.id);
+
+            let mut is_finished = false;
+
+            if real_grain.is_ok() {
+                is_finished = real_grain.unwrap().finished;
+                if is_finished {
+                    if grain_vector::remove_grain(future_grain.id).is_err() {
+                        panic!("Didn't remove the grain!")
+                    }
+                }
+            }
+
+            !is_finished
+        });
+
+        // check if grains crossed the start time
+        for future_grain in &self.future_vector {
+            if future_grain.start <= self.master_clock_counter {
+                self.activate_grain(future_grain.id).ok().unwrap();
             }
         }
     }
 
-    // size in ms between 1ms..100ms
     pub fn activate_grain(&self, id: usize) -> Result<(), Grain> {
         grain_vector::push_grain(id)
     }
 
-    pub fn schedule_grain(&mut self, id: usize, delay: Duration) -> Result<(), Future> {
+    pub fn schedule_grain(&mut self, id: usize, delay: Duration) -> Result<(), TimeInfo> {
         self.future_vector
-            .push(Future::new(self.master_clock_counter + delay, id))
+            .push(TimeInfo::new(id, self.master_clock_counter + delay))
     }
 }
