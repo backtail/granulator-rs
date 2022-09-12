3540626a2f761f2cb0ad3023a54842870d3187aa
backtail <accounts@maxgenson.de>
2022-09-12 15:54:10 +0200

    add number of grains and offset parameter

--- src/manager.rs
+++ src/manager.rs
@@ -8,6 +8,8 @@ pub struct Granulator {
 
     // parameters
     pub master_volume: f32,
+    pub active_grains: usize,
+    pub offset: f32,
 }
 
 impl Granulator {
@@ -16,6 +18,8 @@ impl Granulator {
             scheduler: Scheduler::new(),
 
             master_volume: 1.0 / MAX_GRAINS as f32,
+            active_grains: 1,
+            offset: 0.0,
         }
     }
 
@@ -30,6 +34,12 @@ impl Granulator {
 
         if volume >= 1.0 {
             self.master_volume = 2.0 / MAX_GRAINS as f32;
+        }
+    }
+
+    pub fn set_active_grains(&mut self, num_grains: usize) {
+        if num_grains > MAX_GRAINS {
+            num_grains = MAX_GRAINS;
         }
     }
 }
