654aaa1a652deb21802c1e84d608f4e8d6a9a242
Max Genson <accounts@maxgenson.de>
2022-09-14 17:50:25 +0200

    uses pointer slices insteal of slices now

--- src/grain.rs
+++ src/grain.rs
@@ -2,8 +2,7 @@ use super::manager::FS;
 use super::source::Source;
 use super::window_function::WindowFunction;
 
-#[derive(Clone, Copy, Debug)]
-pub struct Grain<'a> {
+pub struct Grain {
     // envelope variables
     pub window: WindowFunction,
     pub window_parameter: Option<f32>,
@@ -12,9 +11,10 @@ pub struct Grain<'a> {
 
     // source variables
     pub source: Source,
-    pub source_material: &'a [f32],
-    pub source_position: f32, // between 0..grain_length (in samples)
-    pub source_value: f32,    // between 0..1
+    pub source_material: *const [f32], // slice as pointer
+    pub source_position: f32,          // between 0..grain_length (in samples)
+    pub source_value: f32,             // between 0..1
+    pub pitch: f32,
 
     // grain variables
     pub grain_length: f32, // in samples
@@ -24,8 +24,8 @@ pub struct Grain<'a> {
     pub id: usize,
 }
 
-impl<'a> Grain<'a> {
-    pub fn new(id: usize, source_material: &'a [f32]) -> Self {
+impl Grain {
+    pub fn new(id: usize, source_material: *const [f32]) -> Self {
         Grain {
             window: WindowFunction::Sine,
             window_parameter: None,
@@ -36,8 +36,9 @@ impl<'a> Grain<'a> {
             source_material,
             source_position: 0.0,
             source_value: 0.0,
+            pitch: 1.0,
 
-            grain_length: source_material.len() as f32,
+            grain_length: unsafe { (*source_material).len() } as f32,
             finished: false,
 
             id,
@@ -76,7 +77,7 @@ impl<'a> Grain<'a> {
     pub fn update_source_sample(&mut self) -> f32 {
         if !self.finished {
             // move playhead
-            self.source_position += 1.0;
+            self.source_position += self.pitch;
 
             // wrap around
             if self.source_position >= self.grain_length - 1.0 {
@@ -95,4 +96,26 @@ impl<'a> Grain<'a> {
     pub fn get_next_sample(&mut self) -> f32 {
         self.update_envelope() * self.update_source_sample()
     }
+}
+
+/// dead code
+///
+/// helps with pointer concept
+fn _test() {
+    let mut data = [0_f32; 1000];
+    for i in 0..1000 {
+        data[i] = i as f32;
+    }
+
+    let slice = data.as_slice();
+    let buffer_pointer = &slice[0] as *const f32;
+
+    // create pointer with offset
+    let pointer_offset = unsafe { buffer_pointer.offset(9) };
+
+    // create slice from pointer position
+    let slice_pointer = core::ptr::slice_from_raw_parts(pointer_offset, 10);
+
+    // return value from sllice
+    let _value = unsafe { (*slice_pointer)[3] };
 }
