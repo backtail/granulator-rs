f26a278ab33134e87e5daf0672eb007d3161d9a2
Max Genson <accounts@maxgenson.de>
2022-09-14 17:53:31 +0200

    add unit test for bound checking

--- src/audio_tools/mod.rs
+++ src/audio_tools/mod.rs
@@ -6,3 +6,26 @@ pub const TWO_PI: f32 = 6.2831853071795864;
 pub fn soft_clip(sample: f32) -> f32 {
     sample.atan_norm()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use assert2::*;
+    use heapless::Vec;
+
+    #[test]
+    fn check_bounds() {
+        let mut sample_buffer: Vec<f32, 10> = Vec::new();
+        for i in 0..10 {
+            // range from -2 to 2
+            let value = ((i - 5) as f32) / 5.0;
+            sample_buffer.push(value).unwrap();
+        }
+
+        for sample in sample_buffer {
+            let processed = soft_clip(sample);
+            check!(processed <= (1.0 + f32::EPSILON));
+            check!(processed >= (-1.0 - f32::EPSILON));
+        }
+    }
+}
