9221c76ef28bd78983b77c48d1495eecf90dca4d
backtail <accounts@maxgenson.de>
2022-09-15 18:39:19 +0200

    add dedicated threads for main functions

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,20 +1,49 @@
 // use assert2::*;
-use granulator::*;
-
-use std::sync::mpsc::channel;
-use std::thread;
+use granulator::Granulator;
+use std::sync::{Arc, Mutex};
+use std::thread::{sleep, Builder};
+use std::time::Duration;
 
 #[test]
 fn multi_threading() {
-    let (tx, rx) = channel();
+    let schedule_timer_interval = Duration::from_millis(10);
+    let audio_callback_interval = Duration::from_nanos(1190);
 
-    let sender = thread::spawn(move || {
-        let g = manager::Granulator::new();
-        tx.send(g).expect("Unable to send on channel");
+    let audio_ptr = Arc::new(Mutex::new(Granulator::new()));
+    let scheduler_ptr = audio_ptr.clone();
+
+    let audio_callback = Builder::new()
+        .name("Audio Callback".to_string())
+        .spawn(move || {
+            for _ in 0..100 {
+                // lock
+                {
+                    let mut audio = audio_ptr.lock().unwrap();
+                    audio.get_next_sample();
+                }
+
+                sleep(audio_callback_interval);
+            }
+        });
+
+    let scheduler_thread = Builder::new().name("Scheduler".to_string()).spawn(move || {
+        for _ in 0..10 {
+            // lock
+            {
+                let mut scheduler = scheduler_ptr.lock().unwrap();
+                scheduler.update_scheduler(schedule_timer_interval);
+            }
+
+            sleep(schedule_timer_interval);
+        }
     });
 
-    let receiver = thread::spawn(move || rx.recv().expect("Unable to receive from channel"));
-
-    sender.join().expect("The sender thread has panicked");
-    let _sended = receiver.join().expect("The receiver thread has panicked");
+    audio_callback
+        .unwrap()
+        .join()
+        .expect("Audio callback didn't finish!");
+    scheduler_thread
+        .unwrap()
+        .join()
+        .expect("Scheduler didn't finish!");
 }
