1760150dc1b87bb171023c61740cd305b95ceb45
backtail <accounts@maxgenson.de>
2022-09-15 18:29:43 +0200

    tidy API

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 // configure as no_std as default since it is the default feature
 #![cfg_attr(feature = "no_std", no_std)]
 
-pub mod grain;
-pub mod grains_vector;
-pub mod manager;
-pub mod scheduler;
-pub mod source;
-pub mod window_function;
+pub(crate) mod manager;
 
-pub mod audio_tools;
+pub(crate) mod audio_tools;
+pub(crate) mod grain;
+pub(crate) mod grains_vector;
+pub(crate) mod pointer_wrapper;
+pub(crate) mod scheduler;
+pub(crate) mod source;
+pub(crate) mod window_function;
+
+pub use crate::manager::Granulator;
