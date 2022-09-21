a5e9cce8ed95f207be72bcfc193c137d854310c8
Max Genson <accounts@maxgenson.de>
2022-09-21 16:53:45 +0200

    improved master volume control

--- src/manager.rs
+++ src/manager.rs
@@ -61,12 +61,12 @@ impl Granulator {
             self.master_volume = 0.0;
         }
 
-        if volume > 0.0 && volume < 1.0 {
+        if volume > 0.0 && volume < 5.0 {
             self.master_volume = (volume * 2.0) / MAX_GRAINS as f32;
         }
 
-        if volume >= 1.0 {
-            self.master_volume = 2.0 / MAX_GRAINS as f32;
+        if volume >= 5.0 {
+            self.master_volume = 10.0 / MAX_GRAINS as f32;
         }
     }
 
